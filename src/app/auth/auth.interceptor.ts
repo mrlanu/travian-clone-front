e589898abb1afe9316aac9af3f59b23ec5daef1a
mrlanu <mrlanu@gmail.com>
2021-11-23 20:47:46 -0600

    fix: interceptor

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,24 +1,26 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {EMPTY, Observable} from 'rxjs';
 import {AuthService} from "./auth.service";
 import {Injectable} from "@angular/core";
 import {Router} from "@angular/router";
+import {UiService} from "../services/ui.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private uiService: UiService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    if (!this.authService.currentUser?.token){
-      this.router.navigate(['/welcome-page', 'login']);
-    }
-
-    // requesting signup
+    // requesting signup or login
     if (!this.authService.currentUser) {
       return next.handle(req);
+    } else if (!this.authService.currentUser?.token) { // token expired
+      this.authService.logout();
+      this.router.navigate(['/welcome-page', 'login']);
+      this.uiService.showSnackbar('Token expired', null, 4000);
+      return EMPTY;
     }
 
     const newReq = req.clone({
