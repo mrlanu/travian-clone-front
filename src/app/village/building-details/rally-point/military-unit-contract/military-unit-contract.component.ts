c95521425aa2c1cff7a70cc5b925498bc6be5060
mrlanu <mrlanu@gmail.com>
2022-02-23 10:16:31 -0600

    feat: add dateCountUp

--- src/app/village/building-details/rally-point/military-unit-contract/military-unit-contract.component.ts
+++ src/app/village/building-details/rally-point/military-unit-contract/military-unit-contract.component.ts
@@ -1,25 +1,46 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Utils} from "../../../../shared/utils";
 import {MilitaryUnitContract} from "../rally-point.component";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-military-unit-contract',
   templateUrl: './military-unit-contract.component.html',
   styleUrls: ['./military-unit-contract.component.css', '../../../../shared/combat-units.css']
 })
-export class MilitaryUnitContractComponent implements OnInit {
+export class MilitaryUnitContractComponent implements OnInit, OnDestroy {
 
   @Input() militaryUnitContract!: MilitaryUnitContract;
   imgSrc = "../../../../../assets/img/x.gif";
+  arrivalTime: Date | null | undefined;
+  intervalId: number | undefined;
+  arrivalTimeStr = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    this.arrivalTime = new Date(this.militaryUnitContract.arrivalTime!);
+    this.countDateUp();
+  }
 
+  countDateUp(){
+    this.intervalId = setInterval(()=>{
+      this.arrivalTime?.setTime(this.arrivalTime?.getTime() + 1000);
+      let hours = this.arrivalTime!.getHours() < 10 ? `0${this.arrivalTime?.getHours()}` : this.arrivalTime?.getHours().toString();
+      let mins = this.arrivalTime!.getMinutes() < 10 ? `0${this.arrivalTime?.getMinutes()}` : this.arrivalTime?.getMinutes().toString();
+      let secs = this.arrivalTime!.getSeconds() < 10 ? `0${this.arrivalTime?.getSeconds()}` : this.arrivalTime?.getSeconds().toString();
+      this.arrivalTimeStr = `${hours}:${mins}:${secs} `;
+    },1000);
   }
 
   getDuration(): string{
     return Utils.formatTime(this.militaryUnitContract.duration);
   }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+
 
 }
