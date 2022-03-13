f5f8458d9f44ae07e8d29405f3fa15033cb1f749
mrlanu <mrlanu@gmail.com>
2022-03-12 20:25:55 -0600

    refactor: troop movements component

--- src/app/village/troop-movements/troop-movements.component.ts
+++ src/app/village/troop-movements/troop-movements.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {VillageService} from "../../services/village.service";
-import {VillageView} from "../../models/village-dto.model";
-import {map, take} from "rxjs/operators";
+import {ActivatedRoute} from "@angular/router";
 
 export class TroopMovementsResponse {
   constructor(public count: number, public mission: string, public timeToArrive: number) {}
@@ -19,14 +18,15 @@ export class TroopMovementsComponent implements OnInit, OnDestroy {
   movedTroopsList: TroopMovementsResponse[] = [];
   componentSubs: Subscription[] = [];
 
-  constructor(private villageService: VillageService) { }
+  constructor(private villageService: VillageService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.villageService.currentVillage.pipe(take(1)).subscribe(
-      (village: VillageView | null) => {
-        this.villageId = village?.villageId;
-        this.getTroopMovements();
-      });
+    this.route.parent?.params.subscribe(params => {
+      console.log("Params - ", params);
+      this.villageId = params['village-id'];
+      console.log("Id: ", this.villageId);
+      this.getTroopMovements();
+    });
   }
 
   private getTroopMovements() {
@@ -34,6 +34,11 @@ export class TroopMovementsComponent implements OnInit, OnDestroy {
       this.movedTroopsList = res;
       console.log(res);
     });
+  }
+
+  onCountDone() {
+    this.villageService.getVillageById(this.villageId!);
+    this.getTroopMovements();
   }
 
   ngOnDestroy(): void {
